5f2f66748b6474bbc647cfa57b95ca1173887e8d
Roosteridk <36681229+Roosteridk@users.noreply.github.com>
2022-11-24 11:41:42 -0500

    Add files via upload

---
+++ mod.ts
@@ -0,0 +1,150 @@
+export class Deta {
+  private projectId: string;
+  private projectKey: string;
+  rootUrl: URL;
+  auth: Headers;
+  /**
+   * @param projectId The project ID which can be found in Deta dashboard
+   * @param projectKey The project key
+   */
+  constructor(projectId: string, projectKey: string) {
+    this.projectId = projectId;
+    this.projectKey = projectKey;
+    this.auth = new Headers({ "X-API-Key": projectKey });
+    this.auth.append("Content-Type", "application/json");
+    this.rootUrl = new URL(
+      `https://database.deta.sh/v1/${projectId}/`,
+    );
+  }
+  /**
+   * @param name The name of your database
+   * @returns A new database instance
+   */
+  Base<Model>(name: string) {
+    return new Base<Model>(this, name);
+  }
+}
+
+class Base<Model> {
+  name: string;
+  rootUrl: URL;
+  deta: Deta;
+  constructor(deta: Deta, name: string) {
+    this.name = name;
+    this.deta = deta;
+    this.rootUrl = new URL(deta.rootUrl + name);
+  }
+  /**
+   * Stores multiple items in a single request. This request overwrites an item if the key already exists.
+   * @param items A list of items to put
+   * @returns
+   */
+  async put(items: Model[]) {
+    const url = new URL(this.rootUrl + "/items");
+    const body = JSON.stringify({ items });
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: this.deta.auth,
+      body,
+    });
+    const data = await response.json();
+    return data;
+  }
+  /**
+   * @param key The key of the item to fetch
+   * @returns The item with the given key
+   */
+  async get(key: string) {
+    const url = new URL(this.rootUrl + "/items/" + key);
+    const response = await fetch(url, {
+      method: "GET",
+      headers: this.deta.auth,
+    });
+    return response.ok ? await response.json() as Model : null;
+  }
+  /**
+   * @param key The key of the item to delete
+   * @returns
+   */
+  async delete(key: string) {
+    const url = new URL(this.rootUrl + "/items/" + key);
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: this.deta.auth,
+    });
+    const data = await response.json();
+    return data;
+  }
+  /**
+   * Creates a new item only if no item with the same key exists.
+   * @param item The item to update
+   * @returns
+   */
+  async insert(item: Model) {
+    const url = new URL(this.rootUrl + "/items");
+    const body = JSON.stringify({ item });
+    const response = await fetch(url, {
+      method: "POST",
+      headers: this.deta.auth,
+      body,
+    });
+    const data = await response.json();
+    return data;
+  }
+  /**
+   * Updates an item only if an item with key exists.
+   * @param key The key of the item to update
+   * @param updates The updates to make to the item
+   * @returns
+   */
+  async update(key: string, updates: Updates) {
+    const url = new URL(this.rootUrl + "/items/" + key);
+    const body = JSON.stringify(updates);
+    const response = await fetch(url, {
+      method: "PATCH",
+      headers: this.deta.auth,
+      body,
+    });
+    const data = await response.json();
+    return data;
+  }
+  /**
+   * @param query The query to run
+   * @returns The items that match the query
+   */
+  async query(
+    query: unknown[],
+    limit?: number,
+    last?: string,
+  ): Promise<QueryResponse<Model>> {
+    const url = new URL(this.rootUrl + "/query");
+    const response = await fetch(url, {
+      method: "POST",
+      headers: this.deta.auth,
+      body: JSON.stringify({ query, limit, last }),
+    });
+    const data = await response.json();
+    return data;
+  }
+}
+
+interface Updates {
+  /**Fields to update*/
+  set?: Record<string, unknown>;
+  /**Fields to increment.*/
+  increment?: Record<string, number>;
+  /**Fields to append a list of values*/
+  append?: Record<string, unknown[]>;
+  /**Fields to prepend a list of values*/
+  prepend?: Record<string, unknown[]>;
+  /*Fields to remove*/
+  delete?: string[];
+}
+
+interface QueryResponse<Model> {
+  paging: {
+    size: number;
+    last: string;
+  };
+  items: Model[];
+}
